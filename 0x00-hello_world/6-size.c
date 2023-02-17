833ace2caf61fc0f91f928f632383a4a4e4c9543
micky-4u <laryeamichael4u@gmail.com>
2023-02-16 21:28:42 -0800

    types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of char: %ld byte(s)\n", sizeof(char));
+    printf("Size of int: %ld byte(s)\n", sizeof(int));
+    printf("Size of long: %ld byte(s)\n", sizeof(long));
+    printf("Size of long int: %ld byte(s)\n", sizeof(long int));
+    printf("Size of float: %ld byte(s)\n", sizeof(float));
+   
+    return (0);
+}
